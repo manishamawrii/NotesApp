09b2742ab8d09928aee91513aec8e052fb69f401
Manisha Mawri <149868142+manishamawrii@users.noreply.github.com>
2026-02-21 18:54:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
